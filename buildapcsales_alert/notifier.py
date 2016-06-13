12dff408eec0bd99350a76422c5a5ead49464139
ycanastra <ycanastra@gmail.com>
2016-06-13 01:01:47 -0400

    added main

--- buildapcsales_alert/notifier.py
+++ buildapcsales_alert/notifier.py
@@ -6,42 +6,46 @@ import gmail
 import httplib2
 from apiclient import discovery
 
-credentials = gmail.get_credentials()
-http = credentials.authorize(httplib2.Http())
-service = discovery.build('gmail', 'v1', http=http)
+def main():
+	credentials = gmail.get_credentials()
+	http = credentials.authorize(httplib2.Http())
+	service = discovery.build('gmail', 'v1', http=http)
 
-posts = reddit.getNewPosts()
+	posts = reddit.getNewPosts()
 
-productNames = mysql.getAllProductNames()
+	productNames = mysql.getAllProductNames()
 
-stuff = []
+	stuff = []
 
-for item in posts:
-	title = item.title.encode('ascii', 'ignore')
-	for item2 in productNames:
-		if item2 in title:
-			userIDs = mysql.getUserIdsByProductName(item2)
+	for item in posts:
+		title = item.title.encode('ascii', 'ignore')
+		for item2 in productNames:
+			if item2 in title:
+				userIDs = mysql.getUserIdsByProductName(item2)
 
-			for item3 in userIDs:
-				stuff.append((item3, item2, item))
+				for item3 in userIDs:
+					stuff.append((item3, item2, item))
 
 
 
-for item in stuff:
-	userId = item[0]
-	productName = item[1]
-	post = item[2]
+	for item in stuff:
+		userId = item[0]
+		productName = item[1]
+		post = item[2]
 
-	subject = message.createSubject(productName, post)
-	body = message.createBody(productName, post)
-	recipient = mysql.getEmailByUserId(userId)
+		subject = message.createSubject(productName, post)
+		body = message.createBody(productName, post)
+		recipient = mysql.getEmailByUserId(userId)
 
-	if len(recipient) == 0:
-		continue
+		if len(recipient) == 0:
+			continue
 
-	messageToSend = gmail.createMessage('sales.bot.noreply@gmail.com',
-								  recipient,
-								  subject,
-								  body)
+		messageToSend = gmail.createMessage('sales.bot.noreply@gmail.com',
+									  recipient,
+									  subject,
+									  body)
 
-	gmail.sendMessage(service, 'sales.bot.noreply@gmail.com', messageToSend)
+		gmail.sendMessage(service, 'sales.bot.noreply@gmail.com', messageToSend)
+
+if __name__ == "__main__":
+	main()
